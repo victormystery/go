9a631d23ad69bcdcae1c9b0c339983a54d970f4b
victormystery <71271215+victormystery@users.noreply.github.com>
2022-06-03 03:17:52 +0530

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn schoolapp.wsgi --log-file -
---
+++ web/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+   path('', views.home, name='home')
+]+ static(settings.MEDIA_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.MEDIA_ROOT)
